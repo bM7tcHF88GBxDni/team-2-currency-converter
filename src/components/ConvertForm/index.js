49b38e86bfe8259751ca98f53cb89d51871a5dd9
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-05-20 14:39:42 +0100

    Implement ConvertForm states and functions for CurrencyInput user input

--- src/components/ConvertForm/index.js
+++ src/components/ConvertForm/index.js
@@ -1,15 +1,30 @@
+import React, { useState } from "react";
+
 import CurrencyInput from "../CurrencyInput";
 import style from "./convertForm.module.css";
 
 const ConvertForm = () => {
+  const [initialCurrency, setInitialCurrency] = useState("GBP");
+  const [newCurrency, setNewCurrency] = useState("GBP");
+
+  function updateInitialCurrency(code) {
+    setInitialCurrency(code);
+    console.log(initialCurrency);
+  }
+
+  function updateNewCurrency(code) {
+    setNewCurrency(code);
+    console.log(newCurrency);
+  }
+
   return (
     <div className={style.container}>
       <p>Amount</p>
       <input type="text" />
       <p>From</p>
-      <CurrencyInput />
+      <CurrencyInput updateCurrency={updateInitialCurrency} />
       <p>To</p>
-      <CurrencyInput />
+      <CurrencyInput updateCurrency={updateNewCurrency} />
       <p>Exchange rate:</p>
 
       <button>Convert</button>
--- src/components/CurrencyInput/index.js
+++ src/components/CurrencyInput/index.js
@@ -1,13 +1,17 @@
-import React from "react";
 import { countries } from "./countries.js";
 
-function CurrencyInput() {
+function CurrencyInput({ updateCurrency }) {
   const currencyCodes = Object.keys(countries);
   const currencyNames = Object.values(countries);
 
   return (
     <>
-      <input list="currencies" />
+      <input
+        list="currencies"
+        onChange={(e) => {
+          updateCurrency(e.target.value);
+        }}
+      />
       <datalist id="currencies">
         {currencyCodes.map((code, index) => (
           <option key={code} value={code}>
